ff54ded38bc689ae3f55586383adf6139f2633eb
thaolaptrinh <thaolaptrinh@gmail.com>
2026-06-26 17:42:52 +0700

    feat: gateway HTTP server with WebSocket acceptor and health endpoint

--- apps/gateway/main.go
+++ apps/gateway/main.go
@@ -4,20 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/health"
-	"google.golang.org/grpc/health/grpc_health_v1"
 
+	"github.com/thaolaptrinh/spatial-server/pkg/gateway"
 	"github.com/thaolaptrinh/spatial-server/pkg/logging"
-	"github.com/thaolaptrinh/spatial-server/pkg/storage"
 )
 
 func main() {
@@ -32,41 +30,32 @@ func main() {
 	}
 
 	logger := logging.NewDefault(k.String("service.name"))
-	ctx := logging.WithLogger(context.Background(), logger)
 
-	redisAddr := k.String("redis.addr")
-	redisClient, err := storage.NewRedisClient(redisAddr)
-	if err != nil {
-		logger.Error("redis connection failed", slog.String("error", err.Error()))
-		os.Exit(1)
+	wsPort := k.Int("gateway.ws_port")
+	cache := gateway.NewRouterCache(5 * time.Second)
+	handler := gateway.NewHandler(cache)
+
+	httpServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", wsPort),
+		Handler: handler,
 	}
-	defer redisClient.Close()
 
-	gRPCPort := k.Int("grpc.port")
-	roomServiceAddr := fmt.Sprintf("room-service:%d", gRPCPort)
-	gRPCPool, err := grpc.NewClient(roomServiceAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		logger.Error("grpc dial failed", slog.String("target", roomServiceAddr), slog.String("error", err.Error()))
-		os.Exit(1)
-	}
-	defer gRPCPool.Close()
-
-	healthSrv := health.NewServer()
-	healthSrv.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
+	go func() {
+		logger.Info("gateway HTTP server starting", slog.Int("port", wsPort))
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("http serve error", slog.String("error", err.Error()))
+			os.Exit(1)
+		}
+	}()
 
 	logger.Info("gateway starting",
-		slog.String("ws_port", fmt.Sprintf("%d", k.Int("gateway.ws_port"))),
-		slog.String("room_service", roomServiceAddr),
+		slog.Int("ws_port", wsPort),
 	)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	<-sig
 	logger.Info("gateway shutting down")
+	httpServer.Shutdown(context.Background())
 	logger.Info("gateway stopped")
-
-	_ = ctx
-	_ = gRPCPool
 }
